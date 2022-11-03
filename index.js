6aa956f822c99b5bcb7a530f2ee358b44edaf357
alaminarmanjanina <81838250+alaminarmanjanina@users.noreply.github.com>
2022-11-04 04:30:51 +0600

    git done

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("ema john server is running");
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vycotd7.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const productCollection = client.db("emajohn").collection("products");
+    app.get("/products", async (req, res) => {
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      console.log(page, size);
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor
+        .skip(page * size)
+        .limit(size)
+        .toArray();
+      const count = await productCollection.estimatedDocumentCount();
+      res.send({ count, products });
+    });
+  } finally {
+  }
+}
+run().catch((err) => console.error(err));
+
+app.listen(port, () => {
+  console.log(`ema john running on ${port}`);
+});
